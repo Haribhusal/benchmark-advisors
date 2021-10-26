e2bf5e36674fca7a35c57fca8791e075b644ba88
skj.jo0c <skj.jo0c@gmail.com>
2021-10-26 21:39:43 +0545

    Fix signup for investor and add login for investor

--- actions/investor.js
+++ actions/investor.js
@@ -1,17 +1,31 @@
-import axios from "axios";
-import { INVESTOR_SIGNUP } from "./types";
+import axios from 'axios';
+import { INVESTOR_SIGNUP } from './types';
 
 // SIGNUP USER
 export const signUpInvestor = (data) => (dispatch) => {
-  var form_data = new FormData();
-  for (var key in data) {
-    form_data.append(key, data[key]);
-  }
+    var form_data = new FormData();
+    for (var key in data) {
+        form_data.append(key, data[key]);
+    }
 
-  axios.post("/api/startup/signup/", form_data).then((res) => {
-    dispatch({
-      type: INVESTOR_SIGNUP,
-      payload: res.data,
+    axios.post('/api/investor/signup/', form_data).then((res) => {
+        dispatch({
+            type: INVESTOR_SIGNUP,
+            payload: res.data,
+        });
     });
-  });
+};
+
+// LOGIN INVESTOR
+export const investorLogin = (data) => (dispatch) => {
+    var form_data = new FormData();
+    for (var key in data) {
+        form_data.append(key, data[key]);
+    }
+    axios.post('/api/investor/login', form_data).then((res) => {
+        dispatch({
+            type: LOGIN,
+            payload: res.data,
+        });
+    });
 };
